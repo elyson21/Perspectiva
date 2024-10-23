77b2880019958de091969b1728d7cd32a378bbcc
leiazel.nadores <ieiazel.nadores@journeytech.com.ph>
2024-10-23 12:48:03 +0800

    update test

--- perspectiva/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: /perspectiva/login/admin_login.php"); // Redirect to login page
-exit();
-?>
- 
